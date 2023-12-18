11693cc5eb8523dd66dda5461e1eb4627be217ea
LvarunL <varunvyas312@gmail.com>
2023-12-18 15:41:30 +0530

    adding unit tests

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask, request, jsonify
 from flask_restx import Api, Resource, fields
-from config import DevConfig
+from config import DevConfig, TestConfig
 from models import Recipe, User
 from exts import db
 from flask_migrate import Migrate
@@ -11,8 +12,11 @@ from flask_jwt_extended import JWTManager
 def create_app(config):
 
     app = Flask(__name__)
+    
+
     app.config.from_object(config)
 
+    
     db.init_app(app)
 
     migrate = Migrate(app, db)
